a470c1d46eb929c737b5cd8f552bf96763706486
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 14:42:51 -0500

    Update mapper_pres2.py

--- mapper_pres2.py
+++ mapper_pres2.py
@@ -7,11 +7,11 @@ def load_afinn_word_list (afinn_path):
     afin_lines = afin_data.content.decode("utf-8").splitlines()
 
     afinn_word_list = {}
-    for line in afin_lines:
-        parts = str(line).strip().split('\t')
-        if len(parts) == 2:
-            word, score = parts
-            afinn_word_list[word] = int(score)
+    #for line in afin_lines:
+    #    parts = str(line).strip().split('\t')
+    #    if len(parts) == 2:
+    #        word, score = parts
+    #        afinn_word_list[word] = int(score)
     return afinn_word_list
 
 def main(argv):
