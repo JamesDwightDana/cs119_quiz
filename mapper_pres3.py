92bac6854181757b572e1cd4d81e3b92a64f7aca
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 16:27:28 -0500

    Update mapper_pres3.py

--- mapper_pres3.py
+++ mapper_pres3.py
@@ -6,11 +6,10 @@ def load_afinn_word_list (afinn_path):
     counter = 0
     with open(afinn_path, 'r') as file:
         for line in file:
-            counter = counter + 1
-            #parts = str(line).strip().split('\t')
-            #if len(parts) == 2:
-            #    word, score = parts
-            #    afinn_word_list[word] = int(score)
+            parts = str(line).strip().split('\t')
+            if len(parts) == 2:
+                word, score = parts
+                afinn_word_list[word] = int(score)
     return afinn_word_list
 
 def main(argv):
