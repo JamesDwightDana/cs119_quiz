c76137af404bd6245e9df294434256971502f42c
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 14:31:51 -0500

    Update mapper_pres2.py

--- mapper_pres2.py
+++ mapper_pres2.py
@@ -4,14 +4,10 @@ import os, sys, re, string
 def load_afinn_word_list (afinn_path):
     afinn_word_list = {}
     with open(afinn_path, 'r', encoding='utf-8') as file:
-        for line in file:
-            parts = line.strip().split('\t')
-            if len(parts) == 2:
-                word, score = parts
-                afinn_word_list[word] = int(score)
+        pass
     return afinn_word_list
 
-afinn_path = "/afinn/afinn/data/AFINN-en-165.txt"
+afinn_path = "~/afinn/afinn/data/AFINN-en-165.txt"
 afinn_word_list = load_afinn_word_list(afinn_path)
 
 def main(argv):
