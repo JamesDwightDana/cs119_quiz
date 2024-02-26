abd1da52af64c2ca97218e66d4f5043627f066b1
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 16:44:28 -0500

    Update mapper_pres3.py

--- mapper_pres3.py
+++ mapper_pres3.py
@@ -13,6 +13,29 @@ def load_afinn_word_list (afinn_path):
                 afinn_word_list[word] = int(score)
     return afinn_word_list
 
+def clean_text(text):
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = re.sub('[\d\n]', ' ', text)
+    return text
+
+def calc_valence(text, afinn):
+    #
+    # Write your program here
+    #
+    words = text.split ()
+    val = (0+0j)
+    for word in words:
+        try:
+            val += complex(afinn[word], 1)
+        except:
+            pass
+    return val 
+
+def valence(text):
+    return calc_valence(clean_text(text), afinn_word_list)
+
 def main(argv):
     line = sys.stdin.readline()
     pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
