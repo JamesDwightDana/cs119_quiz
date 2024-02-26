37dc58b893f3732582e77f281db311e570b98338
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 18:05:26 -0500

    Update reducer_pres.py

--- reducer_pres.py
+++ reducer_pres.py
@@ -4,10 +4,39 @@
 from operator import itemgetter
 import sys
 
-current_word = None
-current_count = 0
+
+current_key = None
+current_value = 0
+n_copies = 1
 word = None
 
 # input comes from STDIN
 for line in sys.stdin:
-    print(line)
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    key, value = line.split('\t', 1)
+
+    # convert count (currently a string) to int
+    try:
+        value = float(value)
+    except ValueError:
+        # ignore/discard this line
+        continue
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if current_key == key:
+        current_value += value
+        n_copies += 1
+    else:
+        if current_key:
+            # write result to STDOUT
+            print ('%s\t%s' % (current_key, current_value, current_value/n_copies))
+        current_count = value
+        current_word = word
+        n_copies = 1
+
+# do not forget to output the last word if needed!
+if current_word == word:
+    print ('%s\t%s' % (current_word, current_value/n_copies))
