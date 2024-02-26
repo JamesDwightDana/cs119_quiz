15c267351638831667c21ff0b5c4cdff368f0af7
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 18:54:39 -0500

    Update reducer_pres.py

--- reducer_pres.py
+++ reducer_pres.py
@@ -6,7 +6,9 @@ import sys
 
 current_key = None
 current_value = 0
+current_count = 1
 key = None
+
 
 # input comes from STDIN
 for line in sys.stdin:
@@ -26,13 +28,15 @@ for line in sys.stdin:
     # by key (here: word) before it is passed to the reducer
     if current_key == key:
         current_value += value
+        current_count += 1
     else:
         if current_key:
             # write result to STDOUT
-            print ('%s\t%s' % (current_key, current_value))
+            print ('%s\t%s' % (current_key, current_value, current_count))
         current_value = value
         current_key = key
+        current_count = 1
 
 # do not forget to output the last word if needed!
 if current_key == key:
-    print ('%s\t%s' % (current_key, current_value))
+    print ('%s\t%s' % (current_key, current_value, current_count))
