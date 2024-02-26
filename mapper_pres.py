de7cc3d241b54f749f3d0a4fc961a40687122231
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 13:08:59 -0500

    Update mapper_pres.py

--- mapper_pres.py
+++ mapper_pres.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
                 if len(parts) == 2:
                     word, score = parts
                     afinn_word_list[word] = int(score)
-        print("AFINN OPENED.")
+        print("AFINN OPENED.\t1")
     except:
         pass
     main(sys.argv)
