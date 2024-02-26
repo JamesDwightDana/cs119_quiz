f59e247ec5d3993316990b6e288a5660df3550cc
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 14:37:35 -0500

    Update mapper_pres2.py

--- mapper_pres2.py
+++ mapper_pres2.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 import os, sys, re, string
+import requests
 
 def load_afinn_word_list (afinn_path):
+    afin_data = requests.get(afinn_path)
+    afin_lines = afin_data.content.decode("utf-8").splitlines()
+
     afinn_word_list = {}
-    with open(afinn_path, 'r', encoding='utf-8') as file:
-        for line in file:
-            parts = line.strip().split('\t')
-            if len(parts) == 2:
-                word, score = parts
-                afinn_word_list[word] = int(score)
+    for line in afin_lines:
+        parts = str(line).strip().split('\t')
+        if len(parts) == 2:
+            word, score = parts
+            afinn_word_list[word] = int(score)
     return afinn_word_list
 
-afinn_path = "./AFINN-en-165.txt"
+afinn_path = 'https://raw.githubusercontent.com/fnielsen/afinn/master/afinn/data/AFINN-en-165.txt'
 afinn_word_list = load_afinn_word_list(afinn_path)
 
 def main(argv):
@@ -21,7 +24,6 @@ def main(argv):
         while line:
             for word in pattern.findall(line):
                 print ("LongValueSum:" + word.lower() + "\t" + "1")
-                # x = 1 / random.randint(0,99)
             line = sys.stdin.readline()
     except EOFError as error:
         return None
