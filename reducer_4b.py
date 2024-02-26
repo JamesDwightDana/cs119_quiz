9566370f58df878440a78966f9a960b4568a83fb
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 10:22:26 -0500

    quickfix

--- reducer_4b.py
+++ reducer_4b.py
@@ -8,7 +8,9 @@ import sys
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
-
-    # parse the input we got from mapper.py
-    count, word = line.split('\t', 1)
-    print ('%s\t%s' % (count, word))
+    try:
+        # parse the input we got from mapper.py
+        count, word = line.split('\t', 1)
+        print ('%s\t%s' % (count, word))
+    except:
+        pass
