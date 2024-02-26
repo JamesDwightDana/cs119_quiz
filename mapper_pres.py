b6c22ecb4e4870af6261cee3da5c94ea72d8ba58
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-02-26 13:17:27 -0500

    Update mapper_pres.py

--- mapper_pres.py
+++ mapper_pres.py
@@ -11,8 +11,8 @@ def load_afinn_word_list (afinn_path):
                 afinn_word_list[word] = int(score)
     return afinn_word_list
 
-# afinn_path = "./afinn/afinn/data/AFINN-en-165.txt"
-# afinn_word_list = load_afinn_word_list(afinn_path)
+afinn_path = "./AFINN-en-165.txt"
+afinn_word_list = load_afinn_word_list(afinn_path)
 
 def clean_text(text):
     text = text.lower()
@@ -47,16 +47,4 @@ def main(argv):
         return None
 
 if __name__ == "__main__":
-    afinn_path = "./AFINN-en-165.txt"
-    try:
-        afinn_word_list = {}
-        with open(afinn_path, 'r', encoding='utf-8') as file:
-            for line in file:
-                parts = line.strip().split('\t')
-                if len(parts) == 2:
-                    word, score = parts
-                    afinn_word_list[word] = int(score)
-        print("AFINN OPENED.\t1")
-    except:
-        pass
     main(sys.argv)
