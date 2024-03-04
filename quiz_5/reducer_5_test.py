0a95632188b9fee6261191d2ba38031ee605e62a
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-03-04 14:16:36 -0500

    lets see!

--- quiz_5/reducer_5_test.py
+++ quiz_5/reducer_5_test.py
@@ -14,10 +14,12 @@ def main(argv):
     for line in sys.stdin:
         # split input
         temp, word, file, count = line.strip().split('\t', 3)
-        # convert count to int, store in metadict.
+        # convert count to int, store in metadict if blank, add to metadict if exists.
         try:
             count = int(count)
             if file in metadict:
+                if word in metadict[file]:
+                    count += metadict[file][word]
                 metadict[file].update({word:count})
             else:
                 metadict[file] = {}
@@ -25,6 +27,6 @@ def main(argv):
         except ValueError:
             pass
     print(metadict)
-    
+
 if __name__ == "__main__":
     main(sys.argv)
