3c925a533a149838c769a30b0f37ada41617105c
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-03-06 13:14:39 -0500

    Update mapper_5_test2.py

--- quiz_5/mapper_5_test2.py
+++ quiz_5/mapper_5_test2.py
@@ -15,15 +15,15 @@ def get_filename(filepath):
     if re.findall('[a-z]*_[a-z]*-[0-9]*(?=.txt)',filepath):
         return re.findall('[a-z]*_[a-z]*-[0-9]*(?=.txt)',filepath)[0]
     else:
-        return "placeholder"
+        return os.environ['map_input_file']
     
 def main(argv):
     line = sys.stdin.readline()
     try:
         while line:
             # Get input file.
-            if 'mapreduce_map_input_file' in os.environ:
-                filename = get_filename(os.environ['mapreduce_map_input_file'])
+            if 'map_input_file' in os.environ:
+                filename = get_filename(os.environ['map_input_file'])
             else:
                 filename = "placeholder"
             
