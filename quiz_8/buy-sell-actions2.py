84aa5821d048e22deb298b6b49a873377c9556db
JamesDwightDana <48440355+JamesDwightDana@users.noreply.github.com>
2024-04-08 13:02:08 -0400

    Create buy-sell-actions2.py

---
+++ quiz_8/buy-sell-actions2.py
@@ -0,0 +1,110 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+r"""
+ Counts words in UTF8 encoded, '\n' delimited text received from the network.
+ Usage: structured_network_wordcount.py <hostname> <port>
+   <hostname> and <port> describe the TCP server that Structured Streaming
+   would connect to receive data.
+
+ To run this on your local machine, you need to first run a Netcat server
+    `$ nc -lk 9999`
+ and then run the example
+    `$ bin/spark-submit examples/src/main/python/sql/streaming/structured_network_wordcount.py
+    localhost 9999`
+"""
+import sys, time
+
+import pyspark
+from pyspark.conf import SparkConf
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import Window
+
+def setLogLevel(sc, level):
+    from pyspark.sql import SparkSession
+    spark = SparkSession(sc)
+    spark.sparkContext.setLogLevel(level)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: structured_network_wordcount.py <hostname> <port>", file=sys.stderr)
+        sys.exit(-1)
+
+    print ('Argv', sys.argv)
+    
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    print ('host', type(host), host, 'port', type(port), port)
+
+    sc_bak = SparkContext.getOrCreate()
+    sc_bak.stop()
+    
+    time.sleep(15)
+    print ('Ready to work!')
+
+    ctx = pyspark.SparkContext(appName = "Price Work", master="local[*]")
+    print ('Context', ctx)
+
+    spark = SparkSession(ctx).builder.getOrCreate()
+    sc = spark.sparkContext
+
+    setLogLevel(sc, "WARN")
+
+    print ('Session:', spark)
+    print ('SparkContext', sc)
+    
+    # sc = SparkContext(conf=conf)
+
+    # Create DataFrame representing the stream of input lines from connection to host:port
+    inputstream = spark\
+        .readStream\
+        .format('socket')\
+        .option('host', host)\
+        .option('port', port)\
+        .option("delimiter", "\t") \
+        .load()
+    
+    streaming_prices = inputstream\
+        .select(
+            split(col("value"), "\t")[0].cast('timestamp').alias("stamp"),
+            split(col("value"), "\t")[0].cast('string').alias("date"),
+            split(col("value"), "\t")[1].cast("double").alias("priceg"),
+            split(col("value"), "\t")[2].cast("double").alias("pricem")
+        )
+    
+    streaming_prices_with_watermark = streaming_prices
+
+    # Group the data by window and sender and compute the average of each group
+    goog10Day = streaming_prices \
+                    .select(col("date"),col("priceg")) \
+                    .sort(col("date"))
+
+    # Start running the query that prints the running counts to the console
+    query = goog10Day\
+        .writeStream\
+        .outputMode('complete')\
+        .format('console')\
+        .start()
+        # To print more than 20 lines, add .option("numRows", 100000)\ after format('console')\
+
+    query.awaitTermination()
+
+    # ../Python-3.9.2/bin/spark-submit
